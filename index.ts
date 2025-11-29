78bd08bc842d24bf87737bf66d3199ce87108836
oliver <oliverxiaozhen@outlook.com>
2025-11-29 22:40:58 +0800

    添加多语言测试

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Welcome to the CoCo Community CLI!");
+function welcome() {
+    const lang = (process.env.LANG || 'en_US').split('_')[0];
+    return lang === 'zh' ? '欢迎使用CoCo-Community CLI!' : '"Welcome to the CoCo Community CLI!';
+}
+
+// 使用
+console.log(welcome());
