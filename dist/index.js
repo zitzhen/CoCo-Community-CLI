7b98e8fc4d9d1cec19bd2443713cb07b909b4efc
oliver <oliverxiaozhen@outlook.com>
2025-11-29 22:52:15 +0800

    修复vscode标红的问题及增加新功能

--- dist/index.js
+++ dist/index.js
@@ -1,35 +1,35 @@
-import { exec } from 'child_process';
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const child_process_1 = require("child_process");
 function welcome() {
     const lang = (process.env.LANG || 'en_US').split('_')[0];
     return lang === 'zh' ? '欢迎使用CoCo-Community CLI!' : 'Welcome to the CoCo Community CLI!';
 }
-
 function openGitHubRepo() {
     const repoUrl = 'https://github.com/zitzhen/CoCo-Community-CLI';
     console.log('Repository URL:', repoUrl);
-    
     // 根据操作系统选择合适的命令打开浏览器
-    let command: string;
+    let command;
     if (process.platform === 'darwin') { // macOS
         command = `open ${repoUrl}`;
-    } else if (process.platform === 'win32') { // Windows
+    }
+    else if (process.platform === 'win32') { // Windows
         command = `start ${repoUrl}`;
-    } else { // Linux and others
+    }
+    else { // Linux and others
         command = `xdg-open ${repoUrl}`;
     }
-    
-    exec(command, (error, stdout, stderr) => {
+    (0, child_process_1.exec)(command, (error, stdout, stderr) => {
         if (error) {
             console.error('Failed to open browser:', error.message);
             // 在无法打开浏览器的情况下，至少让用户知道URL
             console.log('Please visit the URL manually:', repoUrl);
-        } else {
+        }
+        else {
             console.log('Opening browser...');
         }
     });
 }
-
 // 检查命令行参数
 if (process.argv.length > 2) {
     const args = process.argv.slice(2);
@@ -38,6 +38,5 @@ if (process.argv.length > 2) {
         process.exit(0);
     }
 }
-
 // 使用
 console.log(welcome());
