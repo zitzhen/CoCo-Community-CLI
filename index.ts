aa77a4236f1a43d4a734003fc62aea2a880c65dd
oliver <oliverxiaozhen@outlook.com>
2025-11-30 12:09:44 +0800

    fix(index): 移除不必要的 process.exit 调用

--- index.ts
+++ index.ts
@@ -90,11 +90,9 @@ if (process.argv.length > 2) {
     const args = process.argv.slice(2);
     if (args.includes('github')) {
         openGitHubRepo();
-        process.exit(0);
     }
     if (args.includes('ssl')) {
         ssl();
-        process.exit(0);
     }
 }
 
